7f902cbf413db5b6ca20b3e1fbf0243dc8e956f3
Thfneves <Thiago_neves1997@hotmail.com>
2024-03-12 14:10:27 -0300

    att

--- Primitivos/const/math/exercicioNumber/let/difvar/trings/index.js
+++
@@ -1,6 +0,0 @@
-//avancando na strin
-//string com aspas simples por ter aspas duplas dentro
-// a  barra invertida/ e usada como escape, espaco.
-// let umaString= "um //texto"
-// na string podemos usar contatenar usando codigo concat que faz a mesma coisa que adicao
-console.log(umaString.concat("um", "lindo", "dia"));
