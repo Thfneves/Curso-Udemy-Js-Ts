c3f109b225e1e01cfacb72c7f8f72a81668ae067
Thfneves <Thiago_neves1997@hotmail.com>
2024-03-13 18:38:18 -0300

    ATT

--- Sessao2/IfElse/Exercicioelse/index.js
+++ Sessao2/IfElse/Exercicioelse/index.js
@@ -1,27 +1,53 @@
-function meuEscopo() {
-  const form = document.querySelector(".form");
-  const resultado = document.querySelector(".resultado");
+const form = document.querySelector(".form"); //linkando com. o form html
 
-  const pessoas = [];
-  function recebeEventoForm(evento) {
-    evento.preventDefault();
-    //aqui usamos o form acima na linha 2 para acessar o dom e especificamos que queremos o .form
-    //abaixo estamos especificando quais elementos do form queremos, puxando eles pela classe.
+form.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+  //acima  addeventlistner esta setando para a pagina nao atualizar e perder os dados que sao. preenchidos, (e) é um evento que esta sendo declarado
+  //enquanto o preventDefault faz esse trabalho
+  const inputPeso = e.target.querySelector(`.peso`);
+  const inputAltura = e.target.querySelector(`.altura`);
+  //e.target(pegar)   queryselector é para puxar as informacoes dessas classes, no html
+  console.log(inputPeso);
+  console.log(inputAltura);
 
-    const nome = form.querySelector(".nome");
-    const peso = form.querySelector(".peso");
-    const altura = form.querySelector(".altura");
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+  if (!peso) {
+    setResultado(`Peso invalido`, false);
+    return;
+  } // aqui o professor comentou um pouco sobre quano a funcao da verdadeira, ela para de rodar, aqui usando o return ele ja pede para
+  // retornar em vez de rodar todo o codigo
 
-    pessoas.push({
-      nome: nome.value,
-      peso: peso.value,
-      altura: altura.value,
-    });
-    console.log(pessoas);
-
-    resultado.innerHTML = `<p> Olá: ${nome.value} seu peso é de  ${peso.value} e sua altura ${altura.value}</p>`;
-    //para que apareca no console.loge preciso usarmos o value no console.log dessa forma o site salva a informacao.
+  if (!altura) {
+    setResultado(`Alturua invalida`, false);
+    return;
   }
-  form.addEventListener("submit", recebeEventoForm);
+  const imc = getImc(peso, altura);
+  console.log(imc);
+});
+
+function getImc(peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
 }
-meuEscopo();
+
+//acima criamos um evento ao botao submit, quando clicamos ele ativa o evento preventdefault.
+function criaP() {
+  const p = document.createElement(`p`);
+  p.classList.add(className);
+  return p;
+  //esse bloco de funcao cria um paragrafo tbm
+}
+//isvalid abaixo, ira definir a cor da resposta, se for verdadeiro ele fica verde, senao vermelho.
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector(`.resultado`);
+  resultado.innerHTML = ``;
+  // const p = document.createElement(`p`);
+  // p.classList.add(`paragrafo-resultado`);
+  // p.innerHTML = `qualquercoisa`;
+  //Essa e uma maneira de criar uma funcao para criar paragrafos e espelhar a resposta no html
+  //resultado.appendChild(p);
+  const p = criaP();
+  p.innerHTML(msg);
+  resultado.appendChild(p);
+}
