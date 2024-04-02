32ef5dfb1bcab4211fbe075ec6c14da3bce72e4b
Thfneves <Thiago_neves1997@hotmail.com>
2024-04-02 15:43:57 -0300

    Callback

--- Sessao3/Callback/main.js
+++ Sessao3/Callback/main.js
@@ -1,10 +1,26 @@
 //quando uma acao finaliza executamos callback para chama-la novamente
-function f1() {
-  console.group(`f1`);
+function rand(min = 1000, max = 3000) {
+  const num = Math.random() * (max - min) + min;
+  return Math.floor(num);
 }
-function f3() {
-  console.group(`f3`);
+
+//setando um timeout para definir o tempo que o bloco de codigo sera executado.
+function f1(callback) {
+  setTimeout(function () {
+    console.log(`f1`);
+  }, rand());
 }
-function f2() {
-  console.group(`f2`);
+function f3(callback) {
+  setTimeout(function () {
+    console.log(`f3`);
+  }, rand());
 }
+function f2(callback) {
+  setTimeout(function () {
+    console.log(`f2`);
+  }, rand());
+}
+f1();
+f2();
+f3();
+console.log(`òla mundo`);
